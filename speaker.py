dbbf6829e2f3113d7425472cb56b37f4613aa5d2
athai026 <athai026@ucr.edu>
2023-05-23 01:07:59 -0700

    set up speaker for rfid scanner

---
+++ speaker.py
@@ -0,0 +1,11 @@
+#reference: https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+p = GPIO.PWM(26, 700) # piano and playback speaker
+p.start(70)
+time.sleep(0.2)
+p.stop()
