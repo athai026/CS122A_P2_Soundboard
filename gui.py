5a40a1369b35db9c029e64fc7ea4a3f534be11bd
Alice Thai <athai026@ucr.edu>
2023-06-04 02:56:46 -0700

    added clear soundboard button, both buttons have actions

--- gui.py
+++ gui.py
@@ -5,6 +5,19 @@ import sv_ttk
 import os
 from tkinter.messagebox import showinfo
 
+numSounds = 0
+def change_label(soundBoard):
+    global numSounds
+    if numSounds < 12:
+        soundBoard[numSounds].configure(text=f'new text: {numSounds}')
+        numSounds += 1
+
+def clear_soundboard(soundBoard):
+    global numSounds
+    for i in range(12):
+        soundBoard[i].configure(text=f'sound {i+1}:\n')
+    numSounds = 0
+
 def add_samples(directory, parent):
     for item in os.listdir(directory):
         path = os.path.join(directory, item)
@@ -32,9 +45,6 @@ soundsList.grid(row=0, column=0, sticky='nsew')
 soundsScroll = ttk.Scrollbar(sounds, orient=tk.VERTICAL, command=soundsList.yview)
 soundsList.configure(yscroll=soundsScroll.set)
 soundsScroll.grid(row=0, column=1, sticky='ns')
-
-addSound = ttk.Button(sounds, text='Add Sound', style='Accent.TButton')
-addSound.grid(row=1, column=0, sticky='s', pady=10)
 
 soundBoardDisplay = ttk.Frame(window)
 soundBoardDisplay.grid(row=0, column=1, sticky='nsew', padx = 20)
@@ -75,6 +85,12 @@ sound11.grid(row=1, column=4, sticky='nsew', padx=20, pady=50)
 sound12 = Label(soundBoardDisplay, text='sound 12:\n', width=15, height=5, borderwidth=3, relief='ridge')
 sound12.grid(row=1, column=5, sticky='nsew', padx=20, pady=50)
 
-# sounds.pack(side=tk.LEFT, anchor=tk.N)
-# soundBoardDisplay.pack()
+soundBoard = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12]
+
+addSound = ttk.Button(sounds, text='Add Sound', style='Accent.TButton', command=lambda:change_label(soundBoard))
+addSound.grid(row=1, column=0, sticky='s', pady=10)
+
+clearSound = ttk.Button(soundBoardDisplay, text='Clear Soundboard', style='Accent.TButton', command=lambda:clear_soundboard(soundBoard))
+clearSound.grid(row=2, column=5, sticky='s', pady=10)
+
 window.mainloop()
