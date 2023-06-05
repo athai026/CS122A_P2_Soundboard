049771b5e7bae7efa20808bc545f8c5625a53820
athai026 <athai026@ucr.edu>
2023-06-05 03:26:09 -0700

    made soundboard more responsive

--- touch.py
+++ touch.py
@@ -26,30 +26,13 @@ pygame.mixer.set_num_channels(12)
 pressed = [False, False, False, False, False, False, False, False, False, False, False, False]
 def gui_sense(soundboard, soundsList, sounds):
     global pressed
-    # sound1 = pygame.mixer.Sound(soundsList.item(soundboard[0])['text'])
-    # pygame.mixer.Channel(0).play(sound1)
-    # for i in range (12):
-    #     if not pygame.mixer.Channel(i).get_busy():
-    #         sounds[i].set_volume(0.0)
-    #         pygame.mixer.Channel(i).play(sounds[i])
     for i in range(12):
         if mpr121[i].value:
             print("Input {} touched!".format(i+1))
             lcd.lcd_string("Input {} touched".format(i), lcd.LCD_LINE_1)
             lcd.lcd_string('', lcd.LCD_LINE_2)
-            # if not pygame.mixer.Channel(i).get_busy():
-            # sounds[i].set_volume(1.0)
-            # pygame.mixer.Channel(i).play(sounds[i])
             if not pressed[i]:
-                # sounds[i].set_volume(1.0)
                 pygame.mixer.Channel(i).play(sounds[i])
                 pressed[i] = True
-            # else: 
-            #     if not pygame.mixer.Channel(i).get_busy():
-            #         sounds[i].set_volume(0.0)
         else:
-            if not pygame.mixer.Channel(i).get_busy():
-                # sounds[i].set_volume(0.0)
-                pressed[i] = False
-    # time.sleep(0.1)  
-    # return pressed
+            pressed[i] = False
