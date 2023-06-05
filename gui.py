a1f070554034e6fdcf2996da34b767594da3978c
athai026 <athai026@ucr.edu>
2023-06-05 03:11:18 -0700

    can play sounds on soundboard

--- gui.py
+++ gui.py
@@ -23,7 +23,7 @@ def add_sound(boardDisplay):
     soundPath = soundsList.item(selected_sound)['text']
     soundName = soundsList.item(selected_sound)['values'][0]
     if numSounds < 12:
-        if soundName[-4:] == '.mp3' or soundName[-4:] == '.wav':
+        if soundName[-4:] == '.ogg':
             for i in range(12):
                 if soundBoard[i] == '0':
                     boardDisplay[i].configure(text=f'sound {str(i+1)}:\n{soundName}')
@@ -39,7 +39,7 @@ def add_sound_spot(boardDisplay, position):
     print(selected_sound)
     soundPath = soundsList.item(selected_sound)['text']
     soundName = soundsList.item(selected_sound)['values'][0]
-    if soundName[-4:] == '.mp3' or soundName[-4:] == '.wav':
+    if soundName[-4:] == '.ogg':
         boardDisplay[position-1].configure(text=f'sound {str(position)}:\n{soundName}')
         soundBoard[position-1] = selected_sound
         print(soundBoard)
@@ -54,19 +54,91 @@ def clear_soundboard(boardDisplay):
 def play_sound():
     selected_sound = soundsList.selection()[0]
     soundPath = soundsList.item(selected_sound)['text']
-    if soundPath[-4:] == '.mp3' or soundPath[-4:] == '.wav':
+    if soundPath[-4:] == '.ogg':
         pygame.mixer.music.load(soundPath)
         pygame.mixer.music.play()
         print(f'played sound {soundPath}')
 
+def load_onto_soundboard(playSound):
+    sounds = []
+    sound1 = pygame.mixer.Sound(soundsList.item(soundBoard[0])['text'])
+    sound1.set_volume(1.0)
+    # pygame.mixer.Channel(0).play(sound1, loops=-1)
+    sounds.append(sound1)
+
+    sound2 = pygame.mixer.Sound(soundsList.item(soundBoard[1])['text'])
+    sound2.set_volume(1.0)
+    # pygame.mixer.Channel(1).play(sound2, loops=-1)
+    sounds.append(sound2)
+
+    sound3 = pygame.mixer.Sound(soundsList.item(soundBoard[2])['text'])
+    sound3.set_volume(1.0)
+    # pygame.mixer.Channel(2).play(sound3, loops=-1)
+    sounds.append(sound3)
+
+    sound4 = pygame.mixer.Sound(soundsList.item(soundBoard[3])['text'])
+    sound4.set_volume(1.0)
+    # pygame.mixer.Channel(3).play(sound4, loops=-1)
+    sounds.append(sound4)
+
+    sound5 = pygame.mixer.Sound(soundsList.item(soundBoard[4])['text'])
+    sound5.set_volume(1.0)
+    # pygame.mixer.Channel(4).play(sound5, loops=-1)
+    sounds.append(sound5)
+
+    sound6 = pygame.mixer.Sound(soundsList.item(soundBoard[5])['text'])
+    sound6.set_volume(1.0)
+    # pygame.mixer.Channel(5).play(sound6, loops=-1)
+    sounds.append(sound6)
+    
+    sound7 = pygame.mixer.Sound(soundsList.item(soundBoard[6])['text'])
+    sound7.set_volume(1.0)
+    # pygame.mixer.Channel(6).play(sound7, loops=-1)
+    sounds.append(sound7)
+    
+    sound8 = pygame.mixer.Sound(soundsList.item(soundBoard[7])['text'])
+    sound8.set_volume(1.0)
+    # pygame.mixer.Channel(7).play(sound8, loops=-1)
+    sounds.append(sound8)
+
+    sound9 = pygame.mixer.Sound(soundsList.item(soundBoard[8])['text'])
+    sound9.set_volume(1.0)
+    # pygame.mixer.Channel(8).play(sound9, loops=-1)
+    sounds.append(sound9)
+
+    sound10 = pygame.mixer.Sound(soundsList.item(soundBoard[9])['text'])
+    sound10.set_volume(1.0)
+    # pygame.mixer.Channel(9).play(sound10, loops=-1)
+    sounds.append(sound10)
+
+    sound11 = pygame.mixer.Sound(soundsList.item(soundBoard[10])['text'])
+    sound11.set_volume(1.0)
+    # pygame.mixer.Channel(10).play(sound11, loops=-1)
+    sounds.append(sound11)
+
+    sound12 = pygame.mixer.Sound(soundsList.item(soundBoard[11])['text'])
+    sound12.set_volume(1.0)
+    # pygame.mixer.Channel(11).play(sound12, loops=-1)
+    sounds.append(sound12)
+
+    # pressed = [False, False, False, False, False, False, False, False, False, False, False, False]
+
+    while playSound:
+        touch.gui_sense(soundBoard, soundsList, sounds)
+        # pressed = touch.gui_sense(soundBoard, soundsList, sounds, pressed)
+        if GPIO.input(19):
+            for x in sounds:
+                x.stop()
+            break
+
 def load_in_soundBoard():
     global soundBoard
     # use rfid.read()
-    soundBoard =rfid.gui_read()
+    soundBoard = rfid.gui_read()
     print('read')
     for i in range(12):
         soundName = soundsList.item(soundBoard[i])['values'][0]
-        boardDisplay[i-1].configure(text=f'sound {str(i)}:\n{soundName}')
+        boardDisplay[i].configure(text=f'sound {str(i+1)}:\n{soundName}')
 
 def save_soundBoard():
     # use rfid.write()
@@ -154,6 +226,12 @@ playSound.grid(row=3, column=0, sticky='s', pady=10)
 clearSound = ttk.Button(soundBoardDisplay, text='Clear Soundboard', style='Accent.TButton', command=lambda:clear_soundboard(boardDisplay))
 clearSound.grid(row=2, column=5, sticky='s', pady=10)
 
+loadSound = ttk.Button(soundBoardDisplay, text='Load Soundboard', style='Accent.TButton', command=lambda:load_onto_soundboard(True))
+loadSound.grid(row=3, column=4, sticky='s', pady=10)
+
+stopSound = ttk.Button(soundBoardDisplay, text='Stop Soundboard', style='Accent.TButton', command=lambda:load_onto_soundboard(False))
+stopSound.grid(row=3, column=5, sticky='s', pady=10)
+
 addButtons = ttk.Frame(window)
 addButtons.grid(row=1, column=0, sticky='nsew', padx = 10)
 
@@ -194,15 +272,16 @@ button12 = ttk.Button(addButtons, text='12', style='Accent.TButton', command=lam
 button12.grid(row=1, column=5, sticky='nsew', padx=3, pady=3)
 
 readWrite = ttk.Frame(window)
-readWrite.grid(row=1, column=1, sticky='nsew', padx = 380)
+readWrite.grid(row=1, column=1, sticky='nsew', padx = 470)
 
-readButton= ttk.Button(readWrite, text='Read', style='Accent.TButton', command=lambda:load_in_soundBoard())
+readButton = ttk.Button(readWrite, text='Read', style='Accent.TButton', command=lambda:load_in_soundBoard())
 readButton.grid(row=0, column=3, sticky='nsew', padx=20, pady=3)
 
-writeButton= ttk.Button(readWrite, text='Write', style='Accent.TButton', command=lambda:save_soundBoard())
+writeButton = ttk.Button(readWrite, text='Write', style='Accent.TButton', command=lambda:save_soundBoard())
 writeButton.grid(row=0, column=4, sticky='nsew', padx=20, pady=3)
 
 lcd.lcd_start()
 lcd.lcd_string('touch ready', lcd.LCD_LINE_1)
 pygame.mixer.init()
+pygame.mixer.set_num_channels(12)  # default is 8
 window.mainloop()
